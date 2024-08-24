2e31e34d5fd911d6884c3cbf529d4496c8930808
Yakiv Ponomarenko <ponomarykov@gmail.com>
2024-08-24 23:49:54 +0300

    Complete e2e tests for auth

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,16 +16,34 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'test11@bla.bla';
+    const email = 'asdlkq4321@akl.com';
 
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({ email, password: 'password' })
+      .send({ email, password: 'alskdfjl' })
       .expect(201)
       .then((res) => {
         const { id, email } = res.body;
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
