c72d6491f1986f7162f392afeddf774a59db9f92
Yakiv Ponomarenko <ponomarykov@gmail.com>
2024-09-14 23:12:38 +0300

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 // eslint-disable-next-line
 const cookieSession = require('cookie-session');
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CarValue example')
+    .setDescription('The CarValue API description')
+    .setVersion('1.0')
+    // .addTag('cars')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: 'CarValue API',
+  });
+
   app.use(cookieSession({ keys: ['alakjshdflkj'] }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -6,30 +6,38 @@ import {
   IsLongitude,
   IsLatitude,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateReportDto {
+  @ApiProperty()
   @IsString()
   make: string;
 
+  @ApiProperty()
   @IsString()
   model: string;
 
+  @ApiProperty()
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number;
 
+  @ApiProperty()
   @IsNumber()
   @Min(0)
   @Max(1000000)
   mileage: number;
 
+  @ApiProperty()
   @IsLongitude()
   lng: number;
 
+  @ApiProperty()
   @IsLatitude()
   lat: number;
 
+  @ApiProperty()
   @IsNumber()
   @Min(0)
   @Max(1000000)
