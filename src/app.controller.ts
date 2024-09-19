6625a45d81dbcbba92f0103d05e84531fc6c3006
Yakiv Ponomarenko <ponomarykov@gmail.com>
2024-09-20 01:39:40 +0300

    Change / message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello() {
-    return {
-      // DB_NAME=db.sqlite
-      // COOKIE_KEY=blablaDev
-      // PORT=3000
-      DB_NAME: this.configService.get<string>('DB_NAME'),
-      COOKIE_KEY: this.configService.get<string>('COOKIE_KEY'),
-      PORT: this.configService.get<string>('PORT'),
-    };
-    // return this.appService.getHello();
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
