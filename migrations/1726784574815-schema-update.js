fd1eab7f99c8e8e4fe87d6d07de9faf6312aa3aa
Yakiv Ponomarenko <ponomarykov@gmail.com>
2024-09-20 01:38:35 +0300

    Add migrations

---
+++ migrations/1726784574815-schema-update.js
@@ -0,0 +1,19 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class SchemaUpdate1726784574815 {
+    name = 'SchemaUpdate1726784574815'
+
+    async up(queryRunner) {
+        await queryRunner.query(`CREATE TABLE "temporary_user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "admin" boolean NOT NULL DEFAULT (0), "newField" boolean NOT NULL DEFAULT ('new'))`);
+        await queryRunner.query(`INSERT INTO "temporary_user"("id", "email", "password", "admin") SELECT "id", "email", "password", "admin" FROM "user"`);
+        await queryRunner.query(`DROP TABLE "user"`);
+        await queryRunner.query(`ALTER TABLE "temporary_user" RENAME TO "user"`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "user" RENAME TO "temporary_user"`);
+        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "admin" boolean NOT NULL DEFAULT (0))`);
+        await queryRunner.query(`INSERT INTO "user"("id", "email", "password", "admin") SELECT "id", "email", "password", "admin" FROM "temporary_user"`);
+        await queryRunner.query(`DROP TABLE "temporary_user"`);
+    }
+}
