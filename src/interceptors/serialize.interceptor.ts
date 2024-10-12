7be4afb208d1572cd8907c19139311469d2adf95
Yakiv Ponomarenko <ponomarykov@gmail.com>
2024-10-12 23:20:59 +0300

    Refactor Serialize interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,29 +6,22 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance, ClassConstructor } from 'class-transformer';
 
-interface ClassConstructor {
-  new (...args: any[]): {};
-}
-
-export function Serialize(dto: ClassConstructor) {
+export function Serialize(dto: ClassConstructor<unknown>) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+  constructor(private dto: ClassConstructor<unknown>) {}
 
   intercept(
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<any> | Promise<Observable<any>> {
-    // console.log('--> perform before route handler', context);
-
     return next.handle().pipe(
       map((data: any) => {
-        // console.log('<-- perform before response', data);
-        return plainToClass(this.dto, data, {
+        return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
